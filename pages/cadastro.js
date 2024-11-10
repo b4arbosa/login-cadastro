5851de2a8f42a0662bcfdaa92071699eb278b6fb
Hugo Duarte <hugodover4@gmail.com>
2024-11-10 17:10:36 -0300

    teste

--- pages/cadastro.js
+++ pages/cadastro.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import { setCookie } from 'cookies-next'
 import { useRouter } from 'next/router'
 
-import styles from '../styles/login.module.css'
+import styles from '../styles/cadastro.module.css'
 
 import LoginCard from "@/styles/src/components/loginCard/loginCard"
 import Input from '@/styles/src/components/loginCard/input/input'
@@ -44,17 +44,48 @@ export default function CadastroPage() {
     }
 
     return (
+
+
         <div className={styles.background}>
-            <LoginCard title="Crie sua conta">
-            <form className={styles.form} onSubmit={handleForm}>
-            <Input type="text" placeholder="Seu nome" required value={formData.name} onChange={(e) => {handleFormEdit(e, 'name')}} />
-            <Input type="email" placeholder="Seu e-mail" required value={formData.email} onChange={(e) => {handleFormEdit(e, 'email')}} />
-            <Input type="password" placeholder="Sua senha" required value={formData.password} onChange={(e) => {handleFormEdit(e, 'password')}} />
-            <Button type="submit">Cadastrar</Button>
-            {error && <p className={styles.error}>{error}</p>}
-            <Link href="/login">Já possui uma conta?</Link>
-            </form>
-            </LoginCard>
+
+            <header className='text-white'>
+                teste
+            </header>
+            <main>
+
+                <section>
+                <LoginCard title="Cadastro">
+                <form className={styles.form} onSubmit={handleForm}>
+                <label htmlFor="nome">Nome e Sobrenome</label>
+                <Input id="nome" type="text" placeholder="Seu nome" required value={formData.name} onChange={(e) => {handleFormEdit(e, 'name')}} />
+
+                <div class="div-label">
+                    <div>
+                <label htmlFor="telefone">Telefone/Celular</label>
+                <Input id="telefone" type="string" placeholder="(11)1111-1111" required value={formData.number} onChange={(e) => {handleFormEdit(e, 'number')}} />
+                    </div>
+                    <div>
+                <label htmlFor="nascimento">Data de Nascimento</label>
+                <Input id='nascimento' type="date" required value={formData.date} onChange={(e) => {handleFormEdit(e, 'date')}} />
+                    </div>
+                </div>
+
+
+                <label htmlFor="email">Endereço de Email</label>
+                <Input id="email" type="email" placeholder="Seu e-mail" required value={formData.email} onChange={(e) => {handleFormEdit(e, 'email')}} />
+                <label htmlFor="senha">Senha</label>
+                <Input id='senha' type="password" placeholder="Sua senha" required value={formData.password} onChange={(e) => {handleFormEdit(e, 'password')}} />
+                <Button type="submit">Cadastrar</Button>
+                {error && <p className={styles.error}>{error}</p>}
+                <Link href="/login">Já possui uma conta?</Link>
+                </form>
+                </LoginCard>
+                </section>
+                <section>
+                    <h1>teste</h1>
+                </section>
+            
+            </main>
         </div>
     )
 }
